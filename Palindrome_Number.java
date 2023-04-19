cb92cbfcce950c8c1dc4fc94e0e16f35aea8e629
RameshRaj <123439729+rameshraj678@users.noreply.github.com>
2023-04-19 13:16:07 +0530

    Update Palindrome_Number.java

--- Palindrome_Number.java
+++ Palindrome_Number.java
@@ -9,20 +9,16 @@ public class Palindrome_Number {
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Enter the Number: ");
 	
-		int num=sc.nextInt();
-		
-				
+		int num=sc.nextInt();		
 		int rev=0;
 			
 		while(num!=0) {
 			
 			rev=rev*10+num%10;
-			num=num/10;
-				
+			num=num/10;			
 		}
-			System.out.println("Reverse number:" +rev);
+			System.out.println("Reverse number:" +rev);			
 			
-			
-		}
+}
 
 }
